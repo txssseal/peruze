1e09da6a311986110433a235b18bd893804529e8
Colton <colton@coltonseal.com>
2014-08-18 15:16:30 -0500

    splash page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peruze::Application.config.secret_key_base = 'f1cb545ce3035568b7cf2e9127773452ad431a640d6d16faab3b7495a44dba35ecd8133884fcf199c91e123cd1a20959423f908c64046e72e3c8fe61c7b1be7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Peruze::Application.config.session_store :cookie_store, key: '_peruze_session'
